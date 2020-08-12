cbf3d75ab7a9f8688c99491676e2da7d9bdbf9f1
makennamartin97 <makennamartin97@yahoo.com>
2020-08-12 03:32:23 -0400

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojo_survey.Models;
+
+namespace dojo_survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("survey")]
+        public IActionResult Submission(Survey yourSurvey)
+        {
+            return RedirectToAction("Results", yourSurvey);
+            // Handle your form submission here
+        }
+        [HttpGet("results")]
+        public IActionResult Results(Survey yourSurvey)
+        {
+            return View(yourSurvey);
+        }
+
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,13 @@
+namespace dojo_survey.Models
+{
+    public class Survey
+    {
+        public string Name {get;set;}
+        public string Age {get;set;}
+        public string Hometown {get;set;}
+        public string Song {get;set;}
+        public string Funfact {get;set;}
+        
+        public string Food {get;set;}
+    }
+}
